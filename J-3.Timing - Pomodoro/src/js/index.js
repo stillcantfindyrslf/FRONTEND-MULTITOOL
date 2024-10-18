96038a1975b74ce9d48c5bd6a2d9e99dd3fc9431
fkpe <ilasavic511@gmail.com>
2024-10-18 10:09:12 +0300

    J-3: - add functionality

---
+++ J-3.Timing - Pomodoro/src/js/index.js
@@ -0,0 +1,7 @@
+import '../css/nullstyle.css';
+import '../css/styles.css';
+import { bodyStyle } from './timer.js';
+import { setupEventHandlers } from './executionScript.js';
+
+bodyStyle();
+setupEventHandlers();
---
+++ J-3.Timing - Pomodoro/src/js/pageElements.js
@@ -0,0 +1,29 @@
+export const typeBtn = document.querySelectorAll('.type-button');
+export const body = document.body;
+
+export const pomodoroMainColor = "rgb(186, 73, 73)";
+export const shortBreakMainColor = "rgb(56, 133, 138)";
+export const longBreakMainColor = "rgb(57, 112, 151)";
+
+export const pomodoroBtn = document.getElementById('pomodoro');
+export const shortBreak = document.getElementById('shortBreak');
+export const longBreak = document.getElementById('longBreak');
+
+export const timerElement = document.querySelector('.timer');
+export const startPauseBtn = document.querySelectorAll('.timer-button');
+export const startBtn = document.getElementById('startBtn');
+export const pauseBtn = document.getElementById('pauseBtn');
+export const resumeBtn = document.getElementById('resumeBtn');
+
+export const selectCustom = document.querySelector('.select-custom');
+export const customForm = document.querySelector('.custom__form');
+export const customBtn = document.querySelector('.custom-button');
+
+export const customPomodoroInput = document.getElementById('customPomodoro');
+export const customShortBreakInput = document.getElementById('customShortBreak');
+export const customLongBreakInput = document.getElementById('customLongBreak');
+export const saveCustomBtn = document.getElementById('saveNewTime');
+
+
+
+
